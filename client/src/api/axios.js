0dcaefadb81fcda863f2e847d60dfbc9d14b0b66
Abhishek Prajapati <worktoabhishek@gmail.com>
2026-05-19 19:01:35 +0530

    change in api

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-<<<<<<< HEAD
   baseURL: "https://task1-production-bab1.up.railway.app/api",
-=======
-  baseURL: "https://task1-xusg.onrender.com/api",
->>>>>>> bc0088d3891a93840f5b7de8f9fe1d2f48331630
 });
 
 
